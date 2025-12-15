1326fa18b230445b4dde26e14ada3e1b2e5879cd
Laura Berge <lauraeberge@gmail.com>
2025-12-15 09:18:57 -0600

    add comment about flask app secret keys

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, session, jsonify, make_response
 app = Flask(__name__)
 app.json.compact = False
 
+# Note: Flask app secret keys should not be publicly accessible for deployed applications
+# You can store secret keys in environment variables and use packages like dotenv
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
 # Build Routes
